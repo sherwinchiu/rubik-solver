e285885ae229b91a733739f9de1da4cfe76d2ac1
Sherwin Chiu <sherwin.chiu89@gmail.com>
2021-01-12 19:21:24 -0500

    Added pins along with output/input

--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+// Control over the stepper motors 
+#define  stepWhite  2
+#define  stepBlue   3
+#define  stepGreen  4
+#define  stepOrange 5
+#define  stepRed    6
+#define  stepYellow 7
 
+#define  dirWhite   8
+#define  dirBlue    9
+#define  dirGreen   10
+#define  dirOrange  11
+#define  dirRed     12
+#define  dirYellow  13
+
+// Control over the color sensors
+#define  sdaPin     A4
+#define  clkPin     A5
+
+const byte fullRevolution = 200;
+void setup() {
+  // All output ports based on Arduino Uno 
+  DDRD = B11111111;
+  DDRB = B11111111;
 }
 
 void loop() {
---
+++ main/rotate.ino
@@ -0,0 +1,8 @@
+// since low, MS1, MS2, MS3 connected to low 
+void rotate_left(uint8_t dir, int steps){
+  
+  
+}
+void rotate_right(uint8_t dir){
+  
+}
