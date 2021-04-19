822059863637a1b53c08ba6a2d48a4fb4eb809a9
Sherwin Chiu <sherwin.chiu89@gmail.com>
2021-04-19 19:51:22 -0400

    Adding saved colors

--- main/color_picker.ino
+++
@@ -1,9 +0,0 @@
-void findColor(){
-    uint16_t r, g, b, c;
-    tcs.getRawData(&r, &g, &b, &c); // reading the rgb values 16bits at a time from the i2c channel
-    
-    Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-    Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-    Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
-    Serial.println(" ");
-}
--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,5 @@
 // Can use accel stepper if needed
 #include <Wire.h>
-#include "Adafruit_TCS34725.h"
 // Control over the stepper motors 
 #define  stepWhite  2
 #define  stepBlue   3
@@ -26,23 +25,32 @@ const byte quarterRotation = 50; // 90 degrees, pi/2 radians
 const byte halfRotation = 100;   // 180 degrees, pi radians
 const byte fullRotation = 200; // 360 degrees, 2pi radians
 
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
+char colors[6][9] = {
+   {'W', 'W', 'W',  // White side 
+    'W', 'W', 'W',
+    'W', 'W', 'W'},
+   {'B', 'B', 'B',  // Blue side
+    'B', 'B', 'B',
+    'B', 'B', 'B'},
+   {'G', 'G', 'G',  // Green side
+    'G', 'G', 'G',
+    'G', 'G', 'G'},
+   {'O', 'O', 'O',  // Orange side
+    'O', 'O', 'O',
+    'O', 'O', 'O'},
+   {'R', 'R', 'R',  // Red side
+    'R', 'R', 'R',
+    'R', 'R', 'R'},
+   {'Y', 'Y', 'Y',  // Yellow side 
+    'Y', 'Y', 'Y',
+    'Y', 'Y', 'Y'},
+};
 void setup() {
     // All output ports based on Arduino Uno 
     for(int i = 2; i < 10; i++){
         pinMode(i, OUTPUT);
     }
-    
-    //PORTD = B11111111;
-    //DDRB = B11111111;
     Serial.begin(9600);
-    //color picker
-    if (tcs.begin()) {
-        Serial.println("Found sensor");
-    } else {
-        Serial.println("No TCS34725 found ... check your connections");
-        while (1);
-    }
 }
 
 void loop() {
